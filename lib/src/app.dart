a0bb3aef39581dac32770193a218e6211078c545
Fatai Salami <faety.sal@gmail.com>
2024-09-02 22:58:34 +0100

    Add home view

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
 class UnitConverter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
       theme: ThemeData(
         useMaterial3: false,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
       ),
-      home: Center(child: Text('Home'),),
+      home: Home()
     );
   }
 }
