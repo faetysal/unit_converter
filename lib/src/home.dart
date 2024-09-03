2fb384da7740e4123be9c281d88b56ef9055cdad
Fatai Salami <faety.sal@gmail.com>
2024-09-03 22:08:25 +0100

    Add gradient to selector

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,7 +1,9 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:gradient_borders/gradient_borders.dart';
 import 'package:hugeicons/hugeicons.dart';
 import 'package:responsive_grid_list/responsive_grid_list.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -40,7 +42,7 @@ class Home extends StatelessWidget {
               separatorBuilder: (context, index) => const SizedBox(width: 8),
               itemBuilder: (context, index) {
                 final quantity = quantities[index];
-                return _buildSelectorItem(quantity);
+                return _buildSelectorItem(quantity, selected: index == 0);
               },
             )
           ),
@@ -56,8 +58,8 @@ class Home extends StatelessWidget {
                         padding: const EdgeInsets.only(top: 8),
                         decoration: BoxDecoration(
                           border: Border(
-                            top: BorderSide(color: Colors.grey),
-                            bottom: BorderSide(color: Colors.grey)
+                            top: BorderSide(color: Colors.grey[300]!),
+                            bottom: BorderSide(color: Colors.grey[300]!)
                           )
                         ),
                         child: Column(
@@ -137,11 +139,6 @@ class Home extends StatelessWidget {
                             ),
                             Container(
                               height: 60,
-                              decoration: BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(color: Colors.grey)
-                                )
-                              ),
                               child: TextField(
                                 textAlign: TextAlign.end,
                                 keyboardType: TextInputType.none,
@@ -178,25 +175,25 @@ class Home extends StatelessWidget {
               horizontalGridSpacing: 0,
               verticalGridSpacing: 0,
               children: [
-                _buildInputBtn('7'),
-                _buildInputBtn('8'),
-                _buildInputBtn('9'),
-                _buildInputBtn('x'),
+                _buildInputBtn(text: '7'),
+                _buildInputBtn(text: '8'),
+                _buildInputBtn(text: '9'),
+                _buildInputBtn(text: '⌫'),
 
-                _buildInputBtn('4'),
-                _buildInputBtn('5'),
-                _buildInputBtn('6'),
-                _buildInputBtn('C'),
+                _buildInputBtn(text: '4'),
+                _buildInputBtn(text: '5'),
+                _buildInputBtn(text: '6'),
+                _buildInputBtn(text: 'C'),
 
-                _buildInputBtn('1'),
-                _buildInputBtn('2'),
-                _buildInputBtn('3'),
-                _buildInputBtn('U'),
+                _buildInputBtn(text: '1'),
+                _buildInputBtn(text: '2'),
+                _buildInputBtn(text: '3'),
+                _buildInputBtn(child: Icon(HugeIcons.strokeRoundedArrowUp01)),
 
-                _buildInputBtn('+/-'),
-                _buildInputBtn('0'),
-                _buildInputBtn('.'),
-                _buildInputBtn('D'),
+                _buildInputBtn(text: '+/-'),
+                _buildInputBtn(text: '0'),
+                _buildInputBtn(text: '.'),
+                _buildInputBtn(child: Icon(HugeIcons.strokeRoundedArrowDown01)),
 
               ],
             ),
@@ -206,27 +203,49 @@ class Home extends StatelessWidget {
     );
   }
 
-  Widget _buildSelectorItem(Quantity q) {
+  Widget _buildSelectorItem(Quantity q, {bool selected = false}) {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 4),
       width: 100,
       height: 40,
       decoration: BoxDecoration(
-        color: Colors.white
+        color: Colors.white,
+        border: selected
+          ? GradientBoxBorder(
+              gradient: LinearGradient(
+                colors: [Colors.pink, Colors.purple],
+              )
+            )
+          : null,
+        borderRadius: BorderRadius.circular(8)
         // border: Border.all(color: Colors.green)
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Icon(q.icon, size: 30,),
+          selected
+            ? RadiantGradientMask(
+              child: Icon(q.icon, size: 30, color: Colors.white,),
+            )
+            : Icon(q.icon, size: 30, color: Colors.grey[600]),
           const SizedBox(height: 4),
           Container(
             alignment: Alignment.center,
             width: double.infinity,
             //color: Colors.grey,
-            child: Text(q.title, style: TextStyle(
+            child: selected 
+              ? GradientText(
+                q.title, 
+                colors: [Colors.pink, Colors.purple],
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+              : Text(q.title, style: TextStyle(
               fontSize: 12,
-              fontWeight: FontWeight.bold
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[600]
             ))
           )
         ],
@@ -234,11 +253,17 @@ class Home extends StatelessWidget {
     );
   }
 
-  Widget _buildInputBtn(String text) {
+  Widget _buildInputBtn({String? text, Widget? child}) {
     return Container(
       width: 80,
       height: 80,
-      child: Center(child: Text(text)),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey[300]!)
+      ),
+      child: child ?? Center(child: Text(text ?? '', style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 18
+      ))),
     );
   }
 }
@@ -255,4 +280,22 @@ class Quantity {
   }): _id = id;
 
   int? get id => _id;
+}
+
+class RadiantGradientMask extends StatelessWidget {
+  RadiantGradientMask({required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (bounds) => RadialGradient(
+        center: Alignment.center,
+        radius: 0.5,
+        colors: [Colors.pink, Colors.purple],
+        tileMode: TileMode.mirror,
+      ).createShader(bounds),
+      child: child,
+    );
+  }
 }
